dda9bb2f593aed7708ca50305a6130b7a0054516
NZSmartie <nzsmartie@gmail.com>
2018-05-06 04:15:23 +1200

    Build a new NavigationDrawer for HomeView

---
+++ src/CoAPExplorer.WPF/Controls/ContentItem.cs
@@ -0,0 +1,19 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CoAPExplorer.WPF.Controls
+{
+    public class ContentItem : HeaderedContentControl
+    {
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
+            nameof(Icon), typeof(PackIconKind), typeof(ContentItem));
+
+        public PackIconKind Icon { get => (PackIconKind)GetValue(IconProperty); set => SetValue(IconProperty, value); }
+    }
+}
